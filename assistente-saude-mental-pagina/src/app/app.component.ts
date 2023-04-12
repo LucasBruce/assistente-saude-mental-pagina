e695f584d2e4620aed4dfe7d23b674a216a047ac
Marcos <siqueira.marcos.04@gmail.com>
2023-04-12 11:14:01 -0300

    dev: página principal feita

--- assistente-saude-mental-pagina/src/app/app.component.ts
+++ assistente-saude-mental-pagina/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'assistente-saude-mental-pagina';
+  title = 'Brain.ly';
 }

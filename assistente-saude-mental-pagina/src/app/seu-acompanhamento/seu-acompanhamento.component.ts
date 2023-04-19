f6c3a2749d2def35abb50f2d74a512e94a0c32a4
Wermerson Washington Cavalcante Avelino <wermersonwca@gmail.com>
2023-04-19 02:49:49 -0300

    Configuração para exibição dos profissionais na tela seu acompanhamento

--- assistente-saude-mental-pagina/src/app/seu-acompanhamento/seu-acompanhamento.component.ts
+++ assistente-saude-mental-pagina/src/app/seu-acompanhamento/seu-acompanhamento.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BackendService } from '../backend.service';
 
 @Component({
   selector: 'app-seu-acompanhamento',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./seu-acompanhamento.component.scss']
 })
 export class SeuAcompanhamentoComponent {
-
+  dados: any;
+  constructor(private backend: BackendService) { }
+  ngOnInit() {
+    this.backend.fazerRequisicao('profissionais').subscribe(
+      dados => {
+        this.dados = dados;
+      },
+      erro => {
+        console.error(erro);
+      }
+    );
+  }
 }

7c4ea0183d3634e40d0d504373ed25a0bd78fb95
Marcos <siqueira.marcos.04@gmail.com>
2023-04-19 02:16:40 -0300

    feature: parametrizando as nformações do card de acompanhamento

--- assistente-saude-mental-pagina/src/app/card-opcao-profissional/card-opcao-profissional.component.ts
+++ assistente-saude-mental-pagina/src/app/card-opcao-profissional/card-opcao-profissional.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-opcao-profissional',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-opcao-profissional.component.scss']
 })
 export class CardOpcaoProfissionalComponent {
+
+  @Input()
+  profissional!: string
+
+  @Input()
+  especialidade!: string;
+
+  @Input()
+  atendimento !: string
 
 }

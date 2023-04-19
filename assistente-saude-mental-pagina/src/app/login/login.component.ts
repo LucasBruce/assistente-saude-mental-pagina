747df37927135821df4a6b9a769c401d75491d38
rodrigodan <rodrigogoncalvesdan@gmail.com>
2023-04-19 18:49:18 -0300

    adding the form system control based on a template driven approach

--- assistente-saude-mental-pagina/src/app/login/login.component.ts
+++ assistente-saude-mental-pagina/src/app/login/login.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+      selector: 'login',
+      templateUrl: './login.component.html',
+      styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+      onSubmit(form: NgForm) {
+            console.log(form);
+            if (!form.valid) {
+                  return;
+            }
+            const username = form.value.username;
+            const password = form.value.password;
+      }
 }
